763aba16da99f63e1a76916481dee4028c6bbdb7
chuyenbka <chuyenbka@gmail.com>
2017-10-04 18:08:06 +0700

    fix comment plugin fb

--- application/views/landingpage/kt4002/index.php
+++ application/views/landingpage/kt4002/index.php
@@ -1020,7 +1020,7 @@ $rs = json_decode($result);
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 col-xs-12">
-                        <div class="fb-comments" data-href="https://lakita.vn/tron-bo-lap-bao-cao-tai-chinh-2017.html" 
+                        <div class="fb-comments" data-href="http://lapbctc2016499.lakita.vn" 
                              data-width="100%" data-numposts="10" data-order-by="reverse_time"></div>
                     </div>
                 </div>
