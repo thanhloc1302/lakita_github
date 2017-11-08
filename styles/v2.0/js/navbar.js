950167a28a52d9f197e6ba1baf294ecb2a5aa1ea
locNT <thanhloc1302@gmail.com>
2017-11-08 16:30:30 +0700

    ok

--- styles/v2.0/js/navbar.js
+++ styles/v2.0/js/navbar.js
@@ -20,12 +20,12 @@ $(function () {
         alert("Bạn chưa có thông báo mới");
         return false;
     });
-  // Add slideDown animation to Bootstrap dropdown when expanding.
+  /* Add slideDown animation to Bootstrap dropdown when expanding.*/
     $('.dropdown').on('show.bs.dropdown', function () {
         $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
     });
 
-    // Add slideUp animation to Bootstrap dropdown when collapsing.
+    /* Add slideUp animation to Bootstrap dropdown when collapsing.*/
     $('.dropdown').on('hide.bs.dropdown', function () {
         $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
     });
